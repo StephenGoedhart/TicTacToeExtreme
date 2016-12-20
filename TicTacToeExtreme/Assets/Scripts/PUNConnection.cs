90a11d468a701b8923d042826149045bde4e0fd1
StephenGoedhart <sdg25@hotmail.com>
2016-12-20 12:41:31 +0100

    All files, including Library data. Need to clean this up later

---
+++ TicTacToeExtreme/Assets/Scripts/PUNConnection.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using Photon;
+
+public class PUNConnection : PunBehaviour {
+
+	public void StartConnection (){
+		Debug.Log ("StartConnection was called...");	
+	}
+
+
+
+
+}
